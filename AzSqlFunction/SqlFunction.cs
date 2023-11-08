29eb03802a1c7ea56e36361ff7320bd72f650937
Dhiraj Bhavsar <dheeraj.89@live.com>
2023-11-08 19:35:40 +0530

    Added Active Directory Default authentication in connection string

--- AzSqlFunction/SqlFunction.cs
+++ AzSqlFunction/SqlFunction.cs
@@ -7,40 +7,47 @@ using AzSqlFunction.Models;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System.Data;
+using System.Web.Http;
+using System;
 
 namespace AzSqlFunction;
 
 public class SqlFunction
 {
-    private readonly ILogger<SqlFunction> _logger;
     private readonly IConfiguration _config;
+    private readonly ILogger<SqlFunction> _logger;
 
     public SqlFunction(ILogger<SqlFunction> logger, IConfiguration config)
     {
-        _logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
-        _config = config ?? throw new System.ArgumentNullException(nameof(config));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _config = config ?? throw new ArgumentNullException(nameof(config));
     }
 
-    [FunctionName("CreateCar")]
+    [FunctionName("Car")]
     public async Task<IActionResult> Create(
         [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] Car car)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
         var id = car.Id;
         _logger.LogInformation("Received Id: {id}", id);
 
         var query = "INSERT INTO [dbo].[Cars] (Id, Name) VALUES(@id, @name)";
 
-        using var connection = new SqlConnection(_config.GetConnectionString("default"));
-        using var command = new SqlCommand(query, connection);
-        command.Parameters.AddWithValue("id", id);
-        command.Parameters.AddWithValue("name", car.Name);
-        if (connection.State != ConnectionState.Open)
-            connection.Open();
+        try
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("default"));
+            using var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("id", id);
+            command.Parameters.AddWithValue("name", car.Name);
+            if (connection.State != ConnectionState.Open)
+                connection.Open();
 
-        var _ = command.ExecuteNonQuery();
+            var _ = await command.ExecuteNonQueryAsync();
+        }
+        catch (Exception ex)
+        {
+            return new ExceptionResult(ex, true);
+        }
 
-        await Task.Yield();
         return new OkObjectResult(id);
     }
 }
